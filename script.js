445a392082418e5853a7d3cf95149327783c8a1a
SobolJa <52502686+SobolJa@users.noreply.github.com>
2020-02-23 22:12:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let num = +prompt('How much?');
+for (let i=0; i < num-1; i++){
+    document.write(`    <div class="row1">
+    <div class='black'></div>
+    <div class='white'></div>
+    <div class='black'></div>
+    <div class='white'></div>
+    <div class='black'></div>
+</div>
+    
+        <div class="row2">
+<div class='white'></div>
+    <div class='black'></div>
+    <div class='white'></div>
+    <div class='black'></div>
+     <div class='white'></div>
+
+    </div>`);
+}
